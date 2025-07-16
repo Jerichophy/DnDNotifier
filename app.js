b3860621acd869917b1db10d3f83312e6967439e
Jerichophy <saavedrajerico@gmail.com>
2025-07-16 14:22:29 +0800

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const webhookUrl = "https://discord.com/api/webhooks/1394696085494169690/7ZOhUsbaArmsYVsRD6U9FUXSNK5k69KZSJ874-ldmEB_mmdwu0e5nXXoqQSTsLI9FUlu";
-console.log("Using latest app1.0.js build");
+console.log("Using latest app1.1.js build");
 let nickname = "";
 let userId = "";
 
